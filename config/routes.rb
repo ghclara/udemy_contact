d2a2832b4008e458d951b7f0bef5fa8bd55add15
Ana Clara <clara.aclr@gmail.com>
2019-01-08 20:36:43 -0200

    crontrole e validações pt-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :contatos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "static_pages#index"
 
   get 'sobre', to: 'static_pages#sobre'
   get 'contato_info', to: 'static_pages#contato_info'
+
+  resources :contatos
+  resources :users, only: [:new, :create]
 end
